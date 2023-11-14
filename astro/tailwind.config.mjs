9abef764d37bb4965b0915f296fd88027a005883
etiry <7997357+etiry@users.noreply.github.com>
2023-11-14 16:03:18 -0500

    Styling updates

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -5,7 +5,12 @@ export default {
 		extend: {
 			colors: {
         'dark-yellow': '#fca311',
+				'med-blue': '#1C829C'
       },
+			fontFamily: {
+				'display': ['Urbanist', 'sans-serif'],
+				'body': ['Roboto', 'sans-serif']
+			}
 		},
 	},
 	plugins: [],
